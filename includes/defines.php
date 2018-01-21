75394c2eaee1ff732a24de56fff5653c422737e6
Anna <anya.kotyuk@gmail.com>
2018-01-21 23:48:42 +0200

    change SITE_PATH definition

--- includes/defines.php
+++ includes/defines.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!defined('SITE_PATH')) define('SITE_PATH', $_SERVER['DIRECTORY_PATH']);
+if (!defined('SITE_PATH')) define('SITE_PATH', $_SERVER['DOCUMENT_ROOT']);
 
 include_once 'db.php';
 include_once 'matches.php';
